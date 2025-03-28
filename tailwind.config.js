d5280707e81937f129a3f71153747e581f110afd
Julia Baur <juliabaur1@gmx.de>
2025-03-28 13:48:56 +0100

    Complete Tailwind CSS + DaisyUI Configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      // Add custom colors or themes here
     },
-    plugins: [require("daisyui")],
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark"], // You can specify themes you want to use
   }
+}
