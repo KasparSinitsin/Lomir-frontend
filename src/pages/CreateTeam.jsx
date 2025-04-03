e0ef069d34fd1b29c35fddec09127ebbc1c59ed4
Julia Baur <juliabaur1@gmx.de>
2025-04-03 15:32:13 +0200

    src/pages/CreateTeam.jsx

---
+++ src/pages/CreateTeam.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TeamCreationForm from '../components/teams/TeamCreationForm';
+import PageContainer from '../components/layout/PageContainer';
+
+const CreateTeam = () => {
+  return (
+    <PageContainer>
+      <TeamCreationForm />
+    </PageContainer>
+  );
+};
+
+export default CreateTeam;
