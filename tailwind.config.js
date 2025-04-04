9b4bd346f9491c2119ba58728564a0c485377586
Julia Baur <juliabaur1@gmx.de>
2025-04-04 17:58:30 +0200

    debugging of tailwind installation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,9 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,css}",
   ],
+  
   theme: {
     extend: {
       // You can add custom colors here if needed
