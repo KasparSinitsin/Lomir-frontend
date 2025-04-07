6e45e8c902adbca8ceb0cc9106769b72b0c6fbc2
Julia Baur <juliabaur1@gmx.de>
2025-04-07 11:50:38 +0200

    added some more breakpoints for media screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,8 +17,10 @@ export default {
       },
     },
     screens: {
+      'xs': '375px', 
       'sm': '640px',
       'md': '768px',
+      'ml': '900px', 
       'lg': '1024px',
       'xl': '1280px',
       '2xl': '1536px',
