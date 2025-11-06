521d29c71e37407c88b16b3856beb67f0a065ee5
Julia Baur <juliabaur1@gmx.de>
2025-11-06 10:59:51 +0100

    Refactor: Extract Team Focus Area section into separate component

--- src/components/teams/TeamDetailsModal.jsx
+++ src/components/teams/TeamDetailsModal.jsx
@@ -15,6 +15,7 @@ import UserDetailsModal from "../users/UserDetailsModal";
 import RoleBadgeDropdown from "./RoleBadgeDropdown";
 import TeamApplicationModal from "./TeamApplicationModal";
 import TeamMembersSection from "./TeamMembersSection";
+import TeamFocusAreaSection from "./TeamFocusAreaSection";
 import axios from "axios";
 import Modal from "../common/Modal";
 
@@ -1000,63 +1001,12 @@ const TeamDetailsModal = ({
                   </div>
                 )}
 
-                {/* Team Tags - Enhanced Display */}
-                <div className="mb-6">
-                  <h3 className="font-semibold text-lg mb-3 flex items-center">
-                    <Tag size={18} className="mr-2 text-primary" />
-                    Team Focus Areas
-                  </h3>
-
-                  {team?.tags?.length > 0 ? (
-                    <div className="space-y-3">
-                      {/* Group tags by category if available */}
-                      {(() => {
-                        const tagsByCategory = {};
-                        team.tags.forEach((tag) => {
-                          const category = tag.category || "Other";
-                          if (!tagsByCategory[category]) {
-                            tagsByCategory[category] = [];
-                          }
-                          tagsByCategory[category].push(tag);
-                        });
-
-                        return Object.entries(tagsByCategory).map(
-                          ([category, categoryTags]) => (
-                            <div
-                              key={category}
-                              className="bg-base-200/30 rounded-lg p-3"
-                            >
-                              <h4 className="font-medium text-sm text-base-content/80 mb-2">
-                                {category}
-                              </h4>
-                              <TagDisplay
-                                tags={categoryTags}
-                                size="md"
-                                variant="primary"
-                                showCategory={false}
-                              />
-                            </div>
-                          )
-                        );
-                      })()}
-                    </div>
-                  ) : (
-                    <div className="bg-base-200/20 rounded-lg p-4 text-center">
-                      <Tag
-                        size={24}
-                        className="mx-auto mb-2 text-base-content/40"
-                      />
-                      <p className="text-sm text-base-content/60">
-                        No focus areas specified yet
-                      </p>
-                      {isCreator && !isEditing && (
-                        <p className="text-xs text-base-content/50 mt-1">
-                          Add tags to help others find your team
-                        </p>
-                      )}
-                    </div>
-                  )}
-                </div>
+                {/* Team Focus Areas */}
+                <TeamFocusAreaSection
+                  team={team}
+                  isEditing={isEditing}
+                  isCreator={isCreator}
+                />
 
                 {/* Team Members */}
                 <TeamMembersSection
