650ed4b20c7871c619b6d8025f7370af4745e7fa
Julia Baur <juliabaur1@gmx.de>
2026-02-16 12:46:36 +0100

    refactor(register): align layout with navbar container + improve responsiveness

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,9 @@
 import RegisterForm from "../components/auth/RegisterForm";
 
-const Register = () => {
-  return <RegisterForm />;
-};
+const Register = () => (
+  <div className="content-container">
+    <RegisterForm />
+  </div>
+);
 
 export default Register;
